e36f07d191be2954a0e64abfa75696fbca4d3b3f
bshags <bryrye99@gmail.com>
2026-08-01 13:27:25 -0400

    fix sitemap namespace

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ Sitemap: https://www.noisefigurecalculator.com/sitemap.xml""", 200, {'Content-Ty
 @app.route('/sitemap.xml')
 def sitemap():
     return """<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/0.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://www.noisefigurecalculator.com/</loc><priority>1.0</priority></url>
   <url><loc>https://www.noisefigurecalculator.com/about</loc><priority>0.8</priority></url>
   <url><loc>https://www.noisefigurecalculator.com/privacy</loc><priority>0.5</priority></url>
