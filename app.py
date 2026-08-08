2fd92742c7d8c0c0ee07b8bedb37cf2f8467a351
bshags <bryrye99@gmail.com>
2026-08-08 13:58:54 -0400

    fix canonical tags to non-www

--- app.py
+++ app.py
@@ -49,15 +49,15 @@ def robots_txt():
     return """User-agent: *
 Allow: /
 
-Sitemap: https://www.noisefigurecalculator.com/sitemap.xml""", 200, {'Content-Type': 'text/plain'}
+Sitemap: https://noisefigurecalculator.com/sitemap.xml""", 200, {'Content-Type': 'text/plain'}
 
 @app.route('/sitemap.xml')
 def sitemap():
     return """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://www.noisefigurecalculator.com/</loc><priority>1.0</priority></url>
-  <url><loc>https://www.noisefigurecalculator.com/about</loc><priority>0.8</priority></url>
-  <url><loc>https://www.noisefigurecalculator.com/privacy</loc><priority>0.5</priority></url>
+  <url><loc>https://noisefigurecalculator.com/</loc><priority>1.0</priority></url>
+  <url><loc>https://noisefigurecalculator.com/about</loc><priority>0.8</priority></url>
+  <url><loc>https://noisefigurecalculator.com/privacy</loc><priority>0.5</priority></url>
 </urlset>""", 200, {'Content-Type': 'application/xml'}
 
 if __name__ == "__main__":
